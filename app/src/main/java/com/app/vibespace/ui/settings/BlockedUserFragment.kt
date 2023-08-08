5ff78e3c42b40f260fefd2e4b716c1abeb9540bd
sahilZimble <sahilj.zimble@gmail.com>
2023-08-08 18:33:49 +0530

    check

--- app/src/main/java/com/app/vibespace/ui/settings/BlockedUserFragment.kt
+++ app/src/main/java/com/app/vibespace/ui/settings/BlockedUserFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.app.vibespace.R
+import com.app.vibespace.databinding.FragmentBlockedUserBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class BlockedUserFragment : Fragment() {
 
-
+   private lateinit var binding:FragmentBlockedUserBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blocked_user, container, false)
+        if(!::binding.isInitialized)
+            binding=DataBindingUtil.inflate(inflater,R.layout.fragment_blocked_user,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
--- app/src/main/java/com/app/vibespace/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/app/vibespace/ui/settings/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.app.vibespace.R
 import com.app.vibespace.databinding.FragmentSettingsBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,13 +21,17 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         if(!::binding.isInitialized)
-            binding=FragmentSettingsBinding.inflate(layoutInflater)
-//            binding=DataBindingUtil.inflate(inflater,R.layout.fragment_settings,container,false)
+         //   binding=FragmentSettingsBinding.inflate(layoutInflater)
+            binding=DataBindingUtil.inflate(inflater,R.layout.fragment_settings,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvBlocked.setOnClickListener {
+            findNavController().navigate(R.id.blockedUserFragment)
+        }
 
     }
 
