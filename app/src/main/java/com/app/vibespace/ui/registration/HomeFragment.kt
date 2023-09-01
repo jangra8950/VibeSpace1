ffa6ec21466ac6d8c4bdd0c9017c00cbf7c93715
sahilZimble <sahilj.zimble@gmail.com>
2023-09-01 13:19:44 +0530

    - code refract in Home fragment.

--- app/src/main/java/com/app/vibespace/ui/registration/HomeFragment.kt
+++ app/src/main/java/com/app/vibespace/ui/registration/HomeFragment.kt
@@ -1,59 +1,41 @@
 package com.app.vibespace.ui.registration
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.DrawableRes
-import androidx.appcompat.content.res.AppCompatResources
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.app.vibespace.Enums.ApiStatus
-import com.app.vibespace.R
-import com.app.vibespace.databinding.FragmentHomeBinding
 import com.app.vibespace.models.profile.UserUpdateModel
 import com.app.vibespace.models.registration.GetPeopleModel
-import com.app.vibespace.ui.profile.UserListProfileFragment
-
 import com.app.vibespace.util.showToast
-import com.app.vibespace.viewModel.profile.ProfileViewModel
 import com.app.vibespace.viewModel.registration.HomeViewModel
-
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapInitOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.MapboxMap
-
 import com.mapbox.maps.plugin.annotation.annotations
 import com.mapbox.maps.plugin.annotation.generated.OnPointAnnotationClickListener
-import com.mapbox.maps.plugin.annotation.generated.PointAnnotation
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
-import com.mapbox.maps.plugin.delegates.listeners.OnMapLoadErrorListener
-import com.mapbox.maps.plugin.lifecycle.lifecycle
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 
-
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
-    var data: UserUpdateModel?=null
-//    private lateinit var binding:FragmentHomeBinding
-    private val model:HomeViewModel by viewModels()
-    private var peopleList=ArrayList<GetPeopleModel.Data.User>()
+    var data: UserUpdateModel? = null
+
+    //    private lateinit var binding:FragmentHomeBinding
+    private val model: HomeViewModel by viewModels()
+    private var peopleList = ArrayList<GetPeopleModel.Data.User>()
     private lateinit var mapView: MapView
     private lateinit var mapboxMap: MapboxMap
 
@@ -64,7 +46,8 @@ class HomeFragment : Fragment() {
         mapView = MapView(
             inflater.context,
             // Use TextureView as render surface for the MapView, for smooth transitions following holding views, e.g. in a ViewPager.
-            MapInitOptions(inflater.context, textureView = true,styleUri=STYLE_URL))
+            MapInitOptions(inflater.context, textureView = true, styleUri = STYLE_URL)
+        )
         return mapView
     }
 
@@ -78,27 +61,26 @@ class HomeFragment : Fragment() {
 
 
     private fun getPeopleList() {
-       activity?.let {
-           model.getPeople().observe(it){response->
-               when(response.status){
-                   ApiStatus.SUCCESS ->{
-                       peopleList.clear()
-                       peopleList.addAll(response.data?.data?.userList!!)
-                       addMap()
-                   }
-                   ApiStatus.ERROR ->{
-                       response.message?.let { it1 -> showToast(requireActivity(), it1) }
-                   }
-                   ApiStatus.LOADING -> {
+        activity?.let {
+            model.getPeople().observe(it) { response ->
+                when (response.status) {
+                    ApiStatus.SUCCESS -> {
+                        peopleList.clear()
+                        peopleList.addAll(response.data?.data?.userList!!)
+                        addMap()
+                    }
 
-                   }
-               }
-           }
-       }
+                    ApiStatus.ERROR -> {
+                        response.message?.let { it1 -> showToast(requireActivity(), it1) }
+                    }
+
+                    ApiStatus.LOADING -> {
+
+                    }
+                }
+            }
+        }
     }
-
-
-
 
 
     private fun addMap() {
@@ -112,15 +94,19 @@ class HomeFragment : Fragment() {
                     val image = loadImageFromUrl(data.mascotIcon)
 
                     if (image != null) {
-                        val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
-                            .withIconImage(image)
-                            .withIconSize(0.3)
-                            .withPoint(point)
+                        val pointAnnotationOptions: PointAnnotationOptions =
+                            PointAnnotationOptions()
+                                .withIconImage(image)
+                                .withIconSize(0.3)
+                                .withPoint(point)
 
                         pointAnnotationManager.create(pointAnnotationOptions)
 
                         pointAnnotationManager.addClickListener(OnPointAnnotationClickListener {
-                            val action=  HomeFragmentDirections.actionHomeFragmentToOtherUserProfileFragment(data = data.userId)
+                            val action =
+                                HomeFragmentDirections.actionHomeFragmentToOtherUserProfileFragment(
+                                    data = data.userId
+                                )
                             findNavController().navigate(action)
                             true
                         })
@@ -131,8 +117,6 @@ class HomeFragment : Fragment() {
             }
         }
     }
-
-
 
 
     private suspend fun loadImageFromUrl(imageUrl: String): Bitmap? = withContext(Dispatchers.IO) {
